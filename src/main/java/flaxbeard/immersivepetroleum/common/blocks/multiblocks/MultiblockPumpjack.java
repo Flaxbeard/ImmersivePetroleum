52ed502d675a998b2e5ea74f9c77412a40706888
Flaxbeard <theflaxbeard@gmail.com>
2017-05-23 20:02:28 -0500

    Update Pumpjack resource list

--- src/main/java/flaxbeard/immersivepetroleum/common/blocks/multiblocks/MultiblockPumpjack.java
+++ src/main/java/flaxbeard/immersivepetroleum/common/blocks/multiblocks/MultiblockPumpjack.java
@@ -407,7 +407,8 @@ public class MultiblockPumpjack implements IMultiblock
 	}
 
 	static final IngredientStack[] materials = new IngredientStack[]{
-			new IngredientStack("scaffoldingSteel", 17),
+			new IngredientStack("scaffoldingSteel", 11),
+			new IngredientStack("fenceTreatedWood", 11),
 			new IngredientStack(new ItemStack(IEContent.blockMetalDevice1, 4, BlockTypes_MetalDevice1.FLUID_PIPE.getMeta())),
 			new IngredientStack(new ItemStack(IEContent.blockMetalDecoration0, 1, BlockTypes_MetalDecoration0.RS_ENGINEERING.getMeta())),
 			new IngredientStack(new ItemStack(IEContent.blockMetalDecoration0, 2, BlockTypes_MetalDecoration0.LIGHT_ENGINEERING.getMeta())),

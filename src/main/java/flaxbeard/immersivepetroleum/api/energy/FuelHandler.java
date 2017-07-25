f7a69effcd9a9fe6c760c5416e5a5351fd968ea1
Flaxbeard <theflaxbeard@gmail.com>
2017-07-25 13:47:33 -0500

    Added config for boat fuel

--- src/main/java/flaxbeard/immersivepetroleum/api/energy/FuelHandler.java
+++ src/main/java/flaxbeard/immersivepetroleum/api/energy/FuelHandler.java
@@ -13,14 +13,39 @@ public class FuelHandler
 	static final HashMap<String, Integer> portableGenAmountTick = new HashMap<String, Integer>();
 	static final HashMap<String, Integer> portableGenPowerTick = new HashMap<String, Integer>();
 
+	static final HashMap<String, Integer> motorboatAmountTick = new HashMap<String, Integer>();
+
+	
 	public static void registerPortableGeneratorFuel(Fluid fuel, int fluxPerTick, int mbPerTick)
 	{
 		if (fuel != null)
 		{
 			portableGenAmountTick.put(fuel.getName(), mbPerTick);
-			portableGenPowerTick.put(fuel.getName(), mbPerTick);
+			portableGenPowerTick.put(fuel.getName(), fluxPerTick);
 		}
 	}
+	
+	public static void registerMotorboatFuel(Fluid fuel, int mbPerTick)
+	{
+		if (fuel != null)
+		{
+			motorboatAmountTick.put(fuel.getName(), mbPerTick);
+		}
+	}
+	
+	public static boolean isValidBoatFuel(Fluid fuel)
+	{
+		if (fuel != null)
+			return motorboatAmountTick.containsKey(fuel.getName());
+		return false;
+	}
+	
+	public static int getBoatFuelUsedPerTick(Fluid fuel)
+	{
+		if (!isValidBoatFuel(fuel)) return 0;
+		return motorboatAmountTick.get(fuel.getName());
+	}
+	
 	
 	public static int getFuelUsedPerTick(Fluid fuel)
 	{

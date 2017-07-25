878fbf38dce640f9288c1484bfecc21feb504aaf
Flaxbeard <theflaxbeard@gmail.com>
2017-07-25 17:05:40 -0500

    Remove lubrication debug code

--- src/main/java/flaxbeard/immersivepetroleum/common/blocks/metal/TileEntityPumpjack.java
+++ src/main/java/flaxbeard/immersivepetroleum/common/blocks/metal/TileEntityPumpjack.java
@@ -507,8 +507,6 @@ public class TileEntityPumpjack extends TileEntityMultiblockMetal<TileEntityPump
 	@Override
 	public boolean canOpenGui()
 	{
-		TileEntityPumpjack master = this.master();
-		LubricatedHandler.lubricateTile(master, 20 * 5);
 		return true;
 	}
 	@Override

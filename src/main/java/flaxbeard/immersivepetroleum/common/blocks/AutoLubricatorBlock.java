c7b2cd2871cbb1b6a2126f4b8732f236b9c887ba
TwistedGate <31029668+TwistedGate@users.noreply.github.com>
2020-08-19 08:26:02 +0200

    Reverted Back to original VoxelShape

--- src/main/java/flaxbeard/immersivepetroleum/common/blocks/AutoLubricatorBlock.java
+++ src/main/java/flaxbeard/immersivepetroleum/common/blocks/AutoLubricatorBlock.java
@@ -1,8 +1,5 @@
 package flaxbeard.immersivepetroleum.common.blocks;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import flaxbeard.immersivepetroleum.ImmersivePetroleum;
 import flaxbeard.immersivepetroleum.common.blocks.metal.AutoLubricatorNewTileEntity;
 import net.minecraft.block.Block;
@@ -41,7 +38,9 @@ public class AutoLubricatorBlock extends IPBlockBase{
 	public AutoLubricatorBlock(String name){
 		super(name, Block.Properties.create(material).hardnessAndResistance(3.0F, 15.0F));
 		
-		setDefaultState(getStateContainer().getBaseState().with(FACING, Direction.NORTH).with(SLAVE, false));
+		setDefaultState(getStateContainer().getBaseState()
+				.with(FACING, Direction.NORTH)
+				.with(SLAVE, false));
 	}
 	
 	@Override
@@ -103,83 +102,12 @@ public class AutoLubricatorBlock extends IPBlockBase{
 		return false;
 	}
 	
-	static final Map<Direction, VoxelShape> SHAPE_CACHE=new HashMap<>();
+	static final VoxelShape SHAPE_SLAVE=VoxelShapes.create(.1875F, 0, .1875F, .8125f, 1, .8125f);
+	static final VoxelShape SHAPE_MASTER=VoxelShapes.create(.0625f, 0, .0625f, .9375f, 1, .9375f);
 	
 	@Override
 	public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context){
-		if(!state.get(SLAVE)){
-			
-			// Bottom Half
-			
-			Direction facing=state.get(FACING);
-			VoxelShape shape=SHAPE_CACHE.get(facing);
-			if(shape==null){
-				
-				VoxelShape vs0=VoxelShapes.create(0.0, 0.0, 0.0, 1.0, 0.1875, 1.0); // Plate at the very bottom
-				
-				VoxelShape vs1=VoxelShapes.empty();
-				{
-					VoxelShape vs1_0=VoxelShapes.create(0.0625, 0.1875, 0.0625, 0.25, 0.75, 0.25); // North-West Leg
-					VoxelShape vs1_1=VoxelShapes.create(0.0625, 0.1875, 0.75, 0.25, 0.75, 0.9375); // West-South Leg
-					VoxelShape vs1_2=VoxelShapes.create(0.75, 0.1875, 0.0625, 0.9375, 0.75, 0.25); // North-East Leg
-					VoxelShape vs1_3=VoxelShapes.create(0.75, 0.1875, 0.75, 0.9375, 0.75, 0.9375); // East-South Leg
-					
-					VoxelShape vs1_4=VoxelShapes.create(0.1875, 0.375, 0.1875, 0.8125, 1.0, 0.8125); // Glass in the center
-					
-					vs1=VoxelShapes.or(vs1_0, vs1_1, vs1_2, vs1_3, vs1_4);
-				}
-				
-				VoxelShape vs2=VoxelShapes.create(0.0, 0.6875, 0.0, 1.0, 0.875, 1.0); // Plate at the top
-				
-				VoxelShape vs3=VoxelShapes.empty();
-				{
-					// These two little grey thingies
-					switch(facing){
-						case NORTH:{
-							VoxelShape vs4_0=VoxelShapes.create(0.375, 0.4375, 0.0, 0.5, 0.5625, 0.1875);
-							VoxelShape vs4_1=VoxelShapes.create(0.5625, 0.4375, 0.0, 0.6875, 0.5625, 0.1875);
-							
-							vs3=VoxelShapes.or(vs4_0, vs4_1);
-							break;
-						}
-						case SOUTH:{
-							VoxelShape vs4_0=VoxelShapes.create(0.3125, 0.4375, 0.8125, 0.4375, 0.5625, 1.0);
-							VoxelShape vs4_1=VoxelShapes.create(0.5, 0.4375, 0.8125, 0.625, 0.5625, 1.0);
-							
-							vs3=VoxelShapes.or(vs4_0, vs4_1);
-							break;
-						}
-						case EAST:{
-							VoxelShape vs4_0=VoxelShapes.create(0.8125, 0.4375, 0.5625, 1.0, 0.5625, 0.6875);
-							VoxelShape vs4_1=VoxelShapes.create(0.8125, 0.4375, 0.375, 1.0, 0.5625, 0.5);
-							
-							vs3=VoxelShapes.or(vs4_0, vs4_1);
-							break;
-						}
-						case WEST:{
-							VoxelShape vs4_0=VoxelShapes.create(0.0, 0.4375, 0.5, 0.1875, 0.5625, 0.625);
-							VoxelShape vs4_1=VoxelShapes.create(0.0, 0.4375, 0.3125, 0.1875, 0.5625, 0.4375);
-							
-							vs3=VoxelShapes.or(vs4_0, vs4_1);
-							break;
-						}
-						default: vs3=VoxelShapes.empty(); break;
-					}
-				}
-				
-				shape=VoxelShapes.or(vs0, vs1, vs2, vs3);
-				SHAPE_CACHE.put(facing, shape);
-			}
-			
-			return shape;
-		}else{
-			
-			// Top Half
-			
-			VoxelShape vs0=VoxelShapes.create(0.1875, 0.0, 0.1875, 0.8125, 0.9375, 0.8125); // Glass Tube
-			VoxelShape vs1=VoxelShapes.create(0.25, 0.9375, 0.25, 0.75, 1.0, 0.75); // IO Port at Top
-			return VoxelShapes.or(vs0, vs1);
-		}
+		return state.get(SLAVE) ? SHAPE_SLAVE : SHAPE_MASTER;
 	}
 	
 	public static class AutoLubricatorBlockItem extends IPBlockItemBase{

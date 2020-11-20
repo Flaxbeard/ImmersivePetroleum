5f320226153fead541d41b25f0d9cfefafe43f0d
TwistedGate <31029668+TwistedGate@users.noreply.github.com>
2020-11-20 13:49:52 +0100

    Changed Asphalt speed thing

--- src/main/java/flaxbeard/immersivepetroleum/common/blocks/AsphaltBlock.java
+++ src/main/java/flaxbeard/immersivepetroleum/common/blocks/AsphaltBlock.java
@@ -8,24 +8,24 @@ import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.util.ITooltipFlag;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.vector.Vector3d;
 import net.minecraft.util.text.IFormattableTextComponent;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.IBlockReader;
-import net.minecraft.world.World;
 import net.minecraftforge.common.ToolType;
 
 public class AsphaltBlock extends IPBlockBase{
-	private static final double SPEED_MULTIPLIER = 1.125D;
+	private static final float SPEED_FACTOR = 1.20F;
 	
 	public AsphaltBlock(){
-		super("asphalt", Block.Properties.create(Material.ROCK).hardnessAndResistance(2.0F, 10.0F).harvestTool(ToolType.PICKAXE).sound(SoundType.STONE));
+		super("asphalt", Block.Properties.create(Material.ROCK).speedFactor(SPEED_FACTOR).hardnessAndResistance(2.0F, 10.0F).harvestTool(ToolType.PICKAXE).sound(SoundType.STONE));
+	}
+	
+	@Override
+	public float getSpeedFactor(){
+		return SPEED_FACTOR;
 	}
 	
 	@Override
@@ -33,24 +33,12 @@ public class AsphaltBlock extends IPBlockBase{
 		if(IPConfig.MISCELLANEOUS.asphalt_speed.get()){
 			IFormattableTextComponent out = new TranslationTextComponent(
 					"desc.immersivepetroleum.flavour.asphalt",
-					String.format(Locale.ENGLISH, "%.1f", (SPEED_MULTIPLIER * 100 - 100)) + "%"
+					String.format(Locale.ENGLISH, "%.1f", (SPEED_FACTOR * 100 - 100)) + "%"
 			).mergeStyle(TextFormatting.GRAY);
 			
 			tooltip.add(out);
 		}
 		
 		super.addInformation(stack, worldIn, tooltip, flagIn);
-	}
-	
-	@Override
-	public void onEntityWalk(World worldIn, BlockPos pos, Entity entityIn){
-		if(IPConfig.MISCELLANEOUS.asphalt_speed.get()){
-			if(entityIn instanceof PlayerEntity){
-				PlayerEntity player = (PlayerEntity) entityIn;
-				Vector3d motion = player.getMotion();
-				double speedMultiplier = SPEED_MULTIPLIER;
-				player.setMotion(new Vector3d(motion.x * speedMultiplier, motion.y * speedMultiplier, motion.z * speedMultiplier));
-			}
-		}
 	}
 }

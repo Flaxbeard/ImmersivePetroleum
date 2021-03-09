0bb580eb1b92a9a9bf4479350557e5d8cac3ad70
TwistedGate <31029668+TwistedGate@users.noreply.github.com>
2021-03-09 13:35:09 +0100

    Removed "null" from Utils.drainFluidContainer

--- src/main/java/flaxbeard/immersivepetroleum/common/blocks/tileentities/DistillationTowerTileEntity.java
+++ src/main/java/flaxbeard/immersivepetroleum/common/blocks/tileentities/DistillationTowerTileEntity.java
@@ -196,7 +196,7 @@ public class DistillationTowerTileEntity extends PoweredMultiblockTileEntity<Dis
 		super.tick();
 		
 		if(this.inventory.get(INV_0) != ItemStack.EMPTY && this.tanks[TANK_INPUT].getFluidAmount() < this.tanks[TANK_INPUT].getCapacity()){
-			ItemStack emptyContainer = Utils.drainFluidContainer(this.tanks[TANK_INPUT], this.inventory.get(INV_0), this.inventory.get(INV_1), null);
+			ItemStack emptyContainer = Utils.drainFluidContainer(this.tanks[TANK_INPUT], this.inventory.get(INV_0), this.inventory.get(INV_1));
 			if(!emptyContainer.isEmpty()){
 				if(!this.inventory.get(INV_1).isEmpty() && ItemHandlerHelper.canItemStacksStack(this.inventory.get(INV_1), emptyContainer)){
 					this.inventory.get(INV_1).grow(emptyContainer.getCount());

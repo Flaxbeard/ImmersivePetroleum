c77abbbbdc92d0b370dbdcc943795ee6ea52eee6
TwistedGate <31029668+TwistedGate@users.noreply.github.com>
2021-05-26 19:52:49 +0200

    Multiblock block registration hack
    Removes the block from IEs list and adds it to IPs list instead, same
    goes for the Block Item

--- src/main/java/flaxbeard/immersivepetroleum/common/blocks/IPMetalMultiblock.java
+++ src/main/java/flaxbeard/immersivepetroleum/common/blocks/IPMetalMultiblock.java
@@ -1,10 +1,16 @@
 package flaxbeard.immersivepetroleum.common.blocks;
 
+import java.util.Iterator;
 import java.util.function.Supplier;
 
+import blusunrize.immersiveengineering.common.IEContent;
+import blusunrize.immersiveengineering.common.blocks.BlockItemIE;
 import blusunrize.immersiveengineering.common.blocks.generic.MultiblockPartTileEntity;
 import blusunrize.immersiveengineering.common.blocks.metal.MetalMultiblockBlock;
 import flaxbeard.immersivepetroleum.ImmersivePetroleum;
+import flaxbeard.immersivepetroleum.common.IPContent;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
 import net.minecraft.state.Property;
 import net.minecraft.tileentity.TileEntityType;
 import net.minecraft.util.ResourceLocation;
@@ -12,6 +18,22 @@ import net.minecraft.util.ResourceLocation;
 public class IPMetalMultiblock<T extends MultiblockPartTileEntity<T>> extends MetalMultiblockBlock<T>{
 	public IPMetalMultiblock(String name, Supplier<TileEntityType<T>> te, Property<?>... additionalProperties){
 		super(name, te, additionalProperties);
+		
+		// Nessesary hacks
+		IEContent.registeredIEBlocks.remove(this);
+		Iterator<Item> it = IEContent.registeredIEItems.iterator();
+		while(it.hasNext()){
+			Item item = it.next();
+			if(item instanceof BlockItemIE && ((BlockItemIE) item).getBlock() == this){
+				it.remove();
+				break;
+			}
+		}
+		
+		IPContent.registeredIPBlocks.add(this);
+		
+		BlockItem bItem = new BlockItemIE(this, new Item.Properties().group(ImmersivePetroleum.creativeTab));
+		IPContent.registeredIPItems.add(bItem.setRegistryName(getRegistryName()));
 	}
 	
 	@Override

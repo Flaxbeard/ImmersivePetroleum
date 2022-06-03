15c08e841ab5f7cdc323c27fefe38dda2235d57c
TwistedGate <31029668+TwistedGate@users.noreply.github.com>
2022-06-03 16:36:32 +0200

    Fixes #81

--- src/main/java/flaxbeard/immersivepetroleum/common/blocks/tileentities/CokerUnitTileEntity.java
+++ src/main/java/flaxbeard/immersivepetroleum/common/blocks/tileentities/CokerUnitTileEntity.java
@@ -355,7 +355,8 @@ public class CokerUnitTileEntity extends PoweredMultiblockTileEntity<CokerUnitTi
 			BlockPos outPos = getBlockPosForPos(Fluid_OUT).offset(getFacing().getOpposite());
 			update |= FluidUtil.getFluidHandler(this.world, outPos, getFacing()).map(out -> {
 				if(this.bufferTanks[TANK_OUTPUT].getFluidAmount() > 0){
-					FluidStack fs = FluidHelper.copyFluid(this.bufferTanks[TANK_OUTPUT].getFluid(), 1000);
+					FluidStack fs = this.bufferTanks[TANK_OUTPUT].getFluid();
+					fs = FluidHelper.copyFluid(fs, Math.min(fs.getAmount(), 250));
 					int accepted = out.fill(fs, FluidAction.SIMULATE);
 					if(accepted > 0){
 						int drained = out.fill(FluidHelper.copyFluid(fs, Math.min(fs.getAmount(), accepted)), FluidAction.EXECUTE);
